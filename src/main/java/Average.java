1e6794b9dd5f8de1f3f234b834f5e31e3b0c43de
JameshaRicketts <jamesharicketts@gmail.com>
2023-02-10 01:51:07 +0000

    passed

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum = 0;
+        for (int number : nums) {
+            sum += number;
+        }
+        return sum/nums.length;
     }
 }
